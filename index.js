7f65f877dbec79261808f8b399951429b2927c75
gamerbenyt <136914442+gamerbenyt@users.noreply.github.com>
2024-08-16 19:33:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Automating CSRP!","DM Me for support!"];
+const statusMessages = ["Playing With The World!,"A Robot Gamer Ben!"];
 
 
 let currentIndex = 0;
